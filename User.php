8fafad2503a3c097794ac4fbe991b1cc8f02fd53
felix-hauger <felix.hauger@laplateforme.io>
2023-01-02 11:23:16 +0100

    User.php - added register function

--- User.php
+++ User.php
@@ -17,7 +17,21 @@ class User
         }
     }
 
+    public function register($login, $password, $email, $firstname, $lastname)
+    {
+        $sql = 'INSERT INTO users (login, password, email, firstname, lastname) VALUES (?, ?, ?, ?, ?)';
+
+        $insert = $this->db->prepare($sql);
+
+        $insert->bind_param('sssss', $login, $password, $email, $firstname, $lastname);
+
+        $insert->execute();
+
+        return [$login, $password, $email, $firstname, $lastname];
+    }
+
 }
 
 $test = new User;
+$test->register('admin', 'admin', 'felix.hauger@laplateforme.io', 'Félix', 'HAUGER');
 var_dump($test);
