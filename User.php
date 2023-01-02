fe6f546c09452ce7f1ca25d54d684b4f242760be
felix-hauger <felix.hauger@laplateforme.io>
2023-01-02 18:26:25 +0100

    User.php->update() - update user infos in db

--- User.php
+++ User.php
@@ -19,15 +19,15 @@ class User
 
     public function register($login, $password, $email, $firstname, $lastname)
     {
-        $sql = 'INSERT INTO users (login, password, email, firstname, lastname) VALUES (?, ?, ?, ?, ?)';
-
-        $insert = $this->db->prepare($sql);
-
-        $hashed_password = password_hash($password, PASSWORD_DEFAULT, ['cost' => 10]);
-
         if (!$checked_email = filter_var($email, FILTER_VALIDATE_EMAIL)) {
             throw new Exception('Erreur : format email invalide');
         }
+
+        $hashed_password = password_hash($password, PASSWORD_DEFAULT, ['cost' => 10]);
+
+        $sql = 'INSERT INTO users (login, password, email, firstname, lastname) VALUES (?, ?, ?, ?, ?)';
+
+        $insert = $this->db->prepare($sql);
 
         $insert->bind_param('sssss', $login, $hashed_password, $checked_email, $firstname, $lastname);
 
@@ -87,6 +87,58 @@ class User
 
     // public function checkCrentials()
 
+    public function update($login, $password, $email, $firstname, $lastname)
+    {
+        if (!$checked_email = filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            throw new Exception('Erreur : format email invalide');
+        }
+
+        $hashed_password = password_hash($password, PASSWORD_DEFAULT, ['cost' => 10]);
+
+        // $logged_id = $_SESSION['id'];
+
+        // update user infos in db
+        $sql = 'UPDATE users SET login = ?, password = ?, email = ?, firstname = ?, lastname = ? WHERE id = ?';
+
+        $update = $this->db->prepare($sql);
+
+        $update->bind_param('sssssi', $login, $hashed_password, $checked_email, $firstname, $lastname, $_SESSION['id']);
+
+        $update->execute();
+
+        // update user infos from db in instance & session
+        // $sql = 'SELECT id, login, password, email, firstname, lastname FROM users WHERE id = ?';
+
+        // $select = $this->db->prepare($sql);
+
+        // $select->bind_param('i', $logged_id);
+
+        // $select->execute();
+
+        // $result = $select->get_result();
+
+        // $user = $result->fetch_assoc();
+
+        // $this->id = $user['id'];
+        // $this->login = $user['login'];
+        // $this->email = $user['email'];
+        // $this->firstname = $user['firstname'];
+        // $this->lastname = $user['lastname'];
+
+        // $_SESSION['login'] = $this->login;
+        // $_SESSION['email'] = $this->email;
+        // $_SESSION['firstname'] = $this->firstname;
+        // $_SESSION['lastname'] = $this->lastname;
+
+        // return [
+        //     $this->id,
+        //     $this->login,
+        //     $this->email,
+        //     $this->firstname,
+        //     $this->lastname
+        // ];
+    }
+
     public function disconnect()
     {
         unset($_SESSION['id']);
@@ -112,7 +164,7 @@ class User
 
 $test = new User;
 // try {
-//     if ($test->register('del', 'del', 'del@del.fr', 'del', 'del')) {
+//     if ($test->register('toto', 'toto', 'toto@toto.fr', 'toto', 'toto')) {
 //         echo 'Inscription réussie';
 //     }
 // } catch (Exception $e) {
@@ -120,7 +172,7 @@ $test = new User;
 // }
 
 try {
-    if ($test->connect('test', 'test')) {
+    if ($test->connect('toto', 'toto')) {
         echo 'Connexion réussie';
     }
 } catch (Exception $e) {
@@ -129,7 +181,16 @@ try {
 
 var_dump($_SESSION);
 
-$test->disconnect();
-// $test->delete();
+try {
+    if ($test->update('tutu', 'tutu', 'tutu@tutu.fr', 'tutu', 'tutu')) {
+        echo 'Mise à jour des informations réussie';
+    }
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
+
 var_dump($_SESSION);
+
+// $test->disconnect();
+// $test->delete();
 // var_dump($test);
