5bc04d391481ba2b73d25865563eb7aead2d7193
felix-hauger <felix.hauger@laplateforme.io>
2023-02-10 14:14:56 +0100

    pdo/login.php - added html form for users to connect

--- pdo/login.php
+++ pdo/login.php
@@ -1,25 +1,40 @@
 <?php
 
-
 require_once 'class/UserPDO.php';
+require_once 'class/Form.php';
 
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
-$login = 'toto';
-$password = 'toto';
+if (isset($_SESSION['user'])) {
 
-$user_pdo = new UserPDO($login, $password);
-
-
-var_dump($user_pdo);
-
-try {
-    if ($user = $user_pdo->connect()) {
-        var_dump($user);
-        echo 'Connexion réussie. Bienvenue ' . $user->_firstname . "\n";
-    }
-} catch (Exception $e) {
-    echo 'Erreur : ' . $e->getMessage();
 }
+
+if (isset($_POST['submit'])) {
+    if (Form::areAllPostsFilled()) {
+        $login = $_POST['login'];
+        $password = $_POST['password'];
+
+        $user_pdo = new UserPDO($login, $password);
+
+        try {
+            if ($user = $user_pdo->connect()) {
+                // var_dump($user);
+                echo 'Connexion réussie. Bienvenue ' . $user->_firstname . "\n";
+            }
+        } catch (Exception $e) {
+            echo 'Erreur : ' . $e->getMessage();
+        }
+    }
+}
+
+var_dump('session', $_SESSION);
+
+?>
+
+<form action="" method="post">
+    <input type="text" name="login" id="login" placeholder="Login ou email">
+    <input type="password" name="password" id="password" placeholder="Mot de passe">
+    <input type="submit" name="submit" value="Connexion">
+</form>
