933861959d8dc4059480dd9318673259a27fe269
felix-hauger <felix.hauger@laplateforme.io>
2023-02-14 13:03:04 +0100

    pdo/index.php - get & display user firstname

--- pdo/index.php
+++ pdo/index.php
@@ -1,2 +1,14 @@
 <?php
 
+
+require_once 'class/UserPDO.php';
+
+session_start();
+
+$logged_user = new UserPDO();
+
+$logged_user->getAllInfos();
+
+?>
+
+<h1>Bonjour <?= $logged_user->getFirstname() ?></h1>
